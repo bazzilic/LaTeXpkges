d76f288dfaabc8e659b567afdf93ce22c8e7758b
Taras Kuzyo <kuzyo.taras@gmail.com>
2016-02-11 10:08:03 +0200

    #1 in the LIST; minor changes

--- LaTeXpkges.py
+++ LaTeXpkges.py
@@ -10,30 +10,22 @@ import random
 import argparse
 from glob import glob
 
-#MAIN_FILE = 'Thesis.tex'
-#PDF_FILE = 'Thesis.pdf'
-#PATH = os.path.join('.','qwerty')
 
-original_md5 = ''
+def list_all_tex_files(path='.', names=[]):
+    return [y for x in os.walk(path) for y in x[1] if any(item in y for item in names)]
 
-occurences = []
-packages_to_delete = []
-
-def list_all_tex_files(path='.'):
-    return [y for x in os.walk(path) for y in glob(os.path.join(x[0], '*.tex'))]
-
-def get_variants(str):
-    # print 'The string is', '`'+str+'`' # DEBUG
-
+def get_variants(s):
+    # print 'The string is', '`'+s+'`' # DEBUG
+  
     # find where comment starts
     comment_ptrn = re.compile(r'([^\\]|^)%')
-    comment_match = re.search(comment_ptrn, str)
-    comment_start = len(str)
+    comment_match = re.search(comment_ptrn, s)
+    comment_start = len(s)
     if comment_match:
         comment_start = comment_match.start()
 
     uspkg_cmd_pttrn = re.compile(r'\\usepackage\s*(?:\[[^\]]*\])?\s*\{([a-zA-Z0-9,\s-]+)\}')
-    matches = uspkg_cmd_pttrn.finditer(str)
+    matches = uspkg_cmd_pttrn.finditer(s)
 
     for match in matches:
         pkgs = match.group(1) # list of packages in the current \usepackage command
@@ -47,17 +39,21 @@ def get_variants(str):
             continue
 
         if pkgs.find(',') == -1: # single package case
-            yield (str[pkgs_start_pos:pkgs_end_pos], str[:max(0,match.start()-1)] + str[match.end():]) # remove the whole \usepackage command
+            yield (s[pkgs_start_pos:pkgs_end_pos], 
+                   s[:max(0,match.start()-1)] + s[match.end():]) # remove the whole \usepackage command
         else: # multiple packages case
             separators = find_all(pkgs, ',')
             last_comma_index = 0
 
             for sep in separators:
-                yield (str[pkgs_start_pos+last_comma_index:pkgs_start_pos+sep], str[:pkgs_start_pos+last_comma_index] + str[pkgs_start_pos+sep+1:])
+                yield (s[pkgs_start_pos+last_comma_index:pkgs_start_pos+sep], 
+                       s[:pkgs_start_pos+last_comma_index] + s[pkgs_start_pos+sep+1:])
                 last_comma_index = sep + 1
 
             # last package
-            yield (str[pkgs_start_pos+last_comma_index:pkgs_end_pos], str[:pkgs_start_pos+last_comma_index-1] + str[pkgs_end_pos:])
+            yield (s[pkgs_start_pos+last_comma_index:pkgs_end_pos],
+                   s[:pkgs_start_pos+last_comma_index-1] + s[pkgs_end_pos:])
+
 
 def find_all(a_str, sub):
     start = 0
@@ -67,8 +63,9 @@ def find_all(a_str, sub):
         yield start
         start += len(sub) # use start += 1 to find overlapping matches
 
+
 def substitute_line_in_file(path, line_no, new_str): # line_no is 1-based
-    bak_ext = '.bak'+str(random.randint(1000,9999))
+    bak_ext = '.bak' + str(random.randint(1000,9999))
     input_object = fileinput.input(path, inplace=True, backup=bak_ext)
     for line in input_object:
         if fileinput.filelineno() == line_no:
@@ -76,9 +73,10 @@ def substitute_line_in_file(path, line_no, new_str): # line_no is 1-based
         else:
             print line.rstrip("\n\r")
 
-    return path+bak_ext
+    return path + bak_ext
 
-def build(dbgout=False, have_bibtex=False):
+
+def build(filename, latex='pdflatex', bibtex=None, dbgout=False):
     if dbgout:
         sys.stdout.write("Building the project.")
         sys.stdout.flush()
@@ -86,30 +84,32 @@ def build(dbgout=False, have_bibtex=False):
     code = 0
 
     with open(os.devnull, 'w') as FNULL:
-        code = code + subprocess.call(["pdflatex", "-interaction=nonstopmode", "-halt-on-error",
-                                       MAIN_FILE[:-4]], stdout=FNULL, stderr=subprocess.STDOUT)  # cut off the `.tex` ext
+        code = code + subprocess.call([latex, "-interaction=nonstopmode", "-halt-on-error",
+                                       filename], stdout=FNULL, stderr=subprocess.STDOUT)  # cut off the `.tex` ext
         if dbgout:
             sys.stdout.write(".")
             sys.stdout.flush()
             
-        if have_bibtex:
-            code = code + subprocess.call(["bibtex", MAIN_FILE[:-4]], stdout=FNULL, stderr=subprocess.STDOUT)
+        if bibtex:
+            code = code + subprocess.call([bibtex, filename], stdout=FNULL, stderr=subprocess.STDOUT)
             if dbgout:
                 sys.stdout.write(".")
                 sys.stdout.flush()
-            code = code + subprocess.call(["pdflatex", "-interaction=nonstopmode", "-halt-on-error",
-                                           MAIN_FILE[:-4]], stdout=FNULL, stderr=subprocess.STDOUT)
+            code = code + subprocess.call([latex, "-interaction=nonstopmode", "-halt-on-error",
+                                           filename], stdout=FNULL, stderr=subprocess.STDOUT)
             if dbgout:
                 sys.stdout.write(".")
                 sys.stdout.flush()
                 
-        code = code + subprocess.call(["pdflatex", "-interaction=nonstopmode", "-halt-on-error",
-                                       MAIN_FILE[:-4]], stdout=FNULL, stderr=subprocess.STDOUT)
+        code = code + subprocess.call([latex, "-interaction=nonstopmode", "-halt-on-error",
+                                       filename], stdout=FNULL, stderr=subprocess.STDOUT)
 
     if dbgout:
         print(" Done!")
 
-    return (code == 0) and os.path.exists(PDF_FILE)
+    pdffile = filename.replace('.tex', '.pdf')
+    return (code == 0) and os.path.exists(pdffile)
+
 
 def file_md5(path, blocksize=65536):
     hasher = hashlib.md5()
@@ -118,10 +118,10 @@ def file_md5(path, blocksize=65536):
     with open(path, 'rb') as infile:
         contents = infile.read()
 
-    re_ID = re.compile(r'/ID\s+\[<[0-9A-Fa-f]+>\s+<[0-9A-Fa-f]+>\]') # pdflatex puts a timestamp in every PDF
-    re_Creation = re.compile(r'/CreationDate\s+\(D:[0-9+\'-]+\)')
-    re_Mod = re.compile(r'/ModDate\s+\(D:[0-9+\'-]+\)')
-
+    # pdflatex puts a timestamp in every PDF
+    re_ID = re.compile(r"/ID\s+\[<[0-9A-Fa-f]+>\s+<[0-9A-Fa-f]+>\]") 
+    re_Creation = re.compile(r"/CreationDate\s+\(D:[0-9+\'-]+\)")
+    re_Mod = re.compile(r"/ModDate\s+\(D:[0-9+\'-]+\)")
 
     contents = re.sub(re_ID, '', contents)
     contents = re.sub(re_Creation, '/CreationDate ()', contents)
@@ -131,10 +131,11 @@ def file_md5(path, blocksize=65536):
         hasher.update(chunk)
 
     return hasher.hexdigest()
+    
 
-def find_occurences(path='.'):
-    global occurences
-    tex_files = list_all_tex_files(path)
+def find_occurences(path='.', names=[]):
+    occurences = []
+    tex_files = glob("*.tex") #list_all_tex_files(path, names)
     if len(tex_files) == 0:
         return
     for line in fileinput.input([os.path.join(path, name) for name in tex_files]):
@@ -144,56 +145,49 @@ def find_occurences(path='.'):
             occ['line_no'] = fileinput.filelineno()
             occ['string'] = line
             occurences.append(occ)
+            
+    return occurences            
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='LaTeXpkges', 
                 description='LaTeXpkges is a package cleanup utility for LaTeX.')
-    parser.add_argument('path', default=os.path.join('.', 'qwerty'),
-                help='Path to the folder (!) with your LaTeX files')
-    parser.add_argument('name', default='Thesis.tex',
+    
+    parser.add_argument('name', default='main.tex',
                 help='Name of the main .tex file, which is passed to pdflatex for compilation')
 
     args = parser.parse_args()
 
-    PATH = args.path
-    if PATH[-1] != os.sep:
-        PATH = PATH + os.sep
-    if not os.path.isdir(PATH):
-        print "Path is required to be a directory, which the supplied first argument is not!"
+    filename = args.name
+    filepath = os.path.dirname(filename)
+    pdffile  = filename.replace('.tex', '.pdf')
+
+    os.chdir(filepath)
+
+    if not os.path.exists(filename):
+        print "The file", filename, "does not exist"
         print "Try to run  LaTeXpkges --help"
         print
-        exit()
+        sys.exit()
 
-    os.chdir(PATH)
-
-    MAIN_FILE = args.name
-
-    if not os.path.exists(MAIN_FILE):
-        print "The file", MAIN_FILE, "does not exist in directory", PATH
-        print "Try to run  LaTeXpkges --help"
-        print
-        exit()
-
-    PDF_FILE = MAIN_FILE[:-3] + 'pdf'
-
-    if not build(dbgout=True):
+    if not build(filename, dbgout=True):
         print "Initial build fails! Finishing now..."
-        exit()
+        sys.exit()
 
-    print "Calculating the original checksum for " + PDF_FILE + "..."
-    original_md5 = file_md5(PDF_FILE)
+    print "Calculating the original checksum for " + pdffile + "..."
+    original_md5 = file_md5(pdffile)
     print "MD5 for the original PDF is", original_md5
 
     print "Looking for package imports in the files...",
-    find_occurences()
+    occurences = find_occurences('.', [filename])
     print len(occurences), "were found."
 
+    packages_to_delete = []
     for occ in occurences:
         for (pkg, variant) in get_variants(occ['string']):
             print "Testing if", pkg, "can be removed...",
             tmp_file = substitute_line_in_file(occ['filename'], occ['line_no'], variant)
-            if build():                        # I don't remember lazy evaluation
-                new_md5 = file_md5(PDF_FILE)   #
+            if build(filename):                # I don't remember lazy evaluation
+                new_md5 = file_md5(pdffile)    #
                 if new_md5 == original_md5:    # rules in python :)
                     packages_to_delete.append(pkg)
                     print "Yep."
@@ -203,12 +197,15 @@ if __name__ == '__main__':
                 print "Nope. Build fails."
             os.remove(occ['filename']) # on Windows you can't rename if the dst exists
             os.rename(tmp_file, occ['filename'])
-            os.remove(PDF_FILE) if os.path.exists(PDF_FILE) else None
+            os.remove(pdffile) if os.path.exists(pdffile) else None
+
 
     print
-    if len(packages_to_delete) > 0:
+    if packages_to_delete:
         print "It looks like it's safe to remove these packages:"
         for pkg in packages_to_delete:
             print "\t"+pkg
     else:
         print "We didn't find packages that are safe to remove."
+        
+        
